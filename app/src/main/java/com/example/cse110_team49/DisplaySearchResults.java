c52673129fc57a9b7e29cc4169ee0e085f26fea4
Adrian Chen <42275000+Zeitgeisttt@users.noreply.github.com>
2022-04-29 16:58:35 -0700

    Search results generated

--- app/src/main/java/com/example/cse110_team49/DisplaySearchResults.java
+++ app/src/main/java/com/example/cse110_team49/DisplaySearchResults.java
@@ -4,11 +4,59 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class DisplaySearchResults extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_search_results);
+
+        Bundle extras = getIntent().getExtras();
+        String input = extras.getString("input");
+
+        Map<String, ZooDataItem.VertexInfo> vInfo = ZooDataItem.loadVertexInfoJSON(this, "sample_node_info.json");
+        Map<String, ArrayList<String>>  reversedVInfo = new HashMap<>();
+        for (Map.Entry<String, ZooDataItem.VertexInfo> entry : vInfo.entrySet()){
+            String key = entry.getKey();
+            ZooDataItem.VertexInfo value = entry.getValue();
+            for (String tagValue: value.tags) {
+                ArrayList<String> s = reversedVInfo.get(tagValue);
+                if (s == null) {
+                    ArrayList<String> newIdList = new ArrayList<>();
+                    newIdList.add(value.id);
+                    reversedVInfo.put(tagValue, newIdList);
+                }
+                else {
+                    if (!s.contains(value.id)) {
+                        s.add(value.id);
+                    }
+                    reversedVInfo.put(tagValue, s);
+                }
+            }
+
+            for (String nameValue: value.name.toLowerCase().split(" ")){
+                ArrayList<String> s = reversedVInfo.get(nameValue);
+                if (s == null) {
+                    ArrayList<String> newIdList = new ArrayList<>();
+                    newIdList.add(value.id);
+                    reversedVInfo.put(nameValue, newIdList);
+                }
+                else {
+                    if (!s.contains(value.id)) {
+                        s.add(value.id);
+                    }
+                    reversedVInfo.put(nameValue, s);
+                }
+            }
+
+        }
+//        ArrayList<String> idList = reversedVInfo.get(input);
+
+        System.out.println(reversedVInfo);
+
     }
 }
--- app/src/main/java/com/example/cse110_team49/MainActivity.java
+++ app/src/main/java/com/example/cse110_team49/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.cse110_team49;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import org.jgrapht.Graph;
 import org.jgrapht.GraphPath;
@@ -20,8 +23,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // "source" and "sink" are graph terms for the start and end
-        String start = "entrance_exit_gate";
-        String goal = "elephant_odyssey";
+//        String start = "entrance_exit_gate";
+//        String goal = "elephant_odyssey";
+
+
 
         // 1. Load the graph...
 //        Graph<String, IdentifiedWeightedEdge> g = ZooDataItem.loadZooGraphJSON("sample_zoo_graph.json");
@@ -36,39 +41,7 @@ public class MainActivity extends AppCompatActivity {
 //            System.out.println("exception");
 //        }
 
-        Map<String, ZooDataItem.VertexInfo> vInfo = ZooDataItem.loadVertexInfoJSON(this, "sample_node_info.json");
-        Map<String, ArrayList<String>>  reversedVInfo = new HashMap<>();
-        for (Map.Entry<String, ZooDataItem.VertexInfo> entry : vInfo.entrySet()){
-            String key = entry.getKey();
-            ZooDataItem.VertexInfo value = entry.getValue();
-            for (String tagValue: value.tags) {
-                ArrayList<String> s = reversedVInfo.get(tagValue);
-                if (s == null) {
-                    ArrayList<String> newIdList = new ArrayList<>();
-                    newIdList.add(value.id);
-                    reversedVInfo.put(tagValue, newIdList);
-                }
-                else {
-                    s.add(value.id);
-                    reversedVInfo.put(tagValue, s);
-                }
-            }
 
-            for (String nameValue: value.name.toLowerCase().split(" ")){
-                ArrayList<String> s = reversedVInfo.get(nameValue);
-                if (s == null) {
-                    ArrayList<String> newIdList = new ArrayList<>();
-                    newIdList.add(value.id);
-                    reversedVInfo.put(nameValue, newIdList);
-                }
-                else {
-                    s.add(value.id);
-                    reversedVInfo.put(nameValue, s);
-                }
-            }
-
-        }
-        System.out.println(reversedVInfo);
 
 
 //        Map<String, ZooDataItem.EdgeInfo> eInfo = ZooDataItem.loadEdgeInfoJSON("sample_edge_info.json");
@@ -85,5 +58,13 @@ public class MainActivity extends AppCompatActivity {
 //                    vInfo.get(g.getEdgeTarget(e).toString()).name);
 //            i++;
 //        }
+    }
+
+    public void setOnClicktoSearch(View view) {
+        Intent search_page = new Intent(this, DisplaySearchResults.class);
+        EditText search_input = findViewById(R.id.searchInput);
+
+        search_page.putExtra("input", search_input.getText());
+        startActivity(search_page);
     }
 }
