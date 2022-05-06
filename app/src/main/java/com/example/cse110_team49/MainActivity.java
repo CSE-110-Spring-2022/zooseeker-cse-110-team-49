825a108acd604263f6a27052f552bcb4b323654e
Jian Qin <qinjianxyz@gmail.com>
2022-05-06 10:06:01 -0700

    setCurrentLocation

--- app/src/main/java/com/example/cse110_team49/MainActivity.java
+++ app/src/main/java/com/example/cse110_team49/MainActivity.java
@@ -2,24 +2,32 @@ package com.example.cse110_team49;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView currentLocation;
+    Dialog dialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,37 +87,91 @@ public class MainActivity extends AppCompatActivity {
 
         }
         List<String> possible_list_AL = new ArrayList<String>();
-
+        List<String> nodeNameList = new ArrayList<String>();
 
         vInfo.forEach((k, v) -> {
             possible_list_AL.addAll(v.tags);
+            nodeNameList.add(v.name);
         });
 
-        List<String> new_possible_list_AL = possible_list_AL.stream()
-                                                                .distinct()
-                                                                .collect(Collectors.toList());
-
-
+        // search list
+        List<String> new_possible_list_AL = possible_list_AL.stream().distinct().collect(Collectors.toList());
         String[] possible_list = new String[new_possible_list_AL.size()];
 
+
+        // all possible location
         possible_list = new_possible_list_AL.toArray(possible_list);
 
+        currentLocation = findViewById(R.id.setCurrentLocation);
+
+        currentLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // Initialize dialog
+               dialog = new Dialog(MainActivity.this);
+
+               // set custom dialog
+               dialog.setContentView(R.layout.search_location_spinner);
+
+               // set custom height and width
+               dialog.getWindow().setLayout(650, 800);
+
+               // set transparent background
+               dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+               // change here !!!!!!
+               ArrayAdapter<String> adapter_curLocation = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, nodeNameList);
+               // show dialog
+               dialog.show();
 
 
+               // Initialize and assign variable
+               EditText editText = dialog.findViewById(R.id.edit_text);
+               ListView listView = dialog.findViewById(R.id.list_view);
 
+
+               // set adapter
+               listView.setAdapter(adapter_curLocation);
+               editText.addTextChangedListener(new TextWatcher() {
+                   @Override
+                   public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                   }
+
+                   @Override
+                   public void onTextChanged(CharSequence s, int start, int before, int count) {
+                       adapter_curLocation.getFilter().filter(s);
+                   }
+
+                   @Override
+                   public void afterTextChanged(Editable s) {
+
+                   }
+               });
+               listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                   @Override
+                   public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                       // when item selected from list
+                       // set selected item on textView
+                       currentLocation.setText(adapter_curLocation.getItem(position));
+
+                       // Dismiss dialog
+                       dialog.dismiss();
+                   }
+               });
+
+            }
+        });
 
         AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.searchInput);
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, possible_list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, possible_list);
         textView.setAdapter(adapter);
-
-
-//        System.out.println(reversedVInfo);
 
         findViewById(R.id.myList).setOnClickListener(view -> {
             Intent intent = new Intent(this, ExhibitListViewActivity.class);
             startActivity(intent);
         });
+
 
 
 //        Map<String, ZooDataItem.EdgeInfo> eInfo = ZooDataItem.loadEdgeInfoJSON("sample_edge_info.json");
