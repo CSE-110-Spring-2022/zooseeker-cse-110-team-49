f881941e72e81af363f5bfb9b8a1f678d43882c5
Jian Qin <qinjianxyz@gmail.com>
2022-05-29 11:56:18 -0700

    new assets

--- app/src/main/java/com/example/cse110_team49/MainActivity.java
+++ app/src/main/java/com/example/cse110_team49/MainActivity.java
@@ -45,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         vInfo = ZooDataItem.loadVertexInfoJSON(this, "sample_node_info.json");
+
         Map<String, ArrayList<String>>  reversedVInfo = new HashMap<>();
         for (Map.Entry<String, ZooDataItem.VertexInfo> entry : vInfo.entrySet()){
             String key = entry.getKey();
--- app/src/main/java/com/example/cse110_team49/ZooDataItem.java
+++ app/src/main/java/com/example/cse110_team49/ZooDataItem.java
@@ -1,17 +1,30 @@
 package com.example.cse110_team49;
 
 import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.room.ColumnInfo;
+import androidx.room.PrimaryKey;
+
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.List;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.lang.reflect.Type;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
-import com.google.gson.Gson;
-import com.google.gson.annotations.SerializedName;
-import com.google.gson.reflect.TypeToken;
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultUndirectedWeightedGraph;
 import org.jgrapht.nio.json.JSONImporter;
@@ -19,6 +32,7 @@ import org.jgrapht.nio.json.JSONImporter;
 
 
 public class ZooDataItem {
+
     public static class VertexInfo {
         public static enum Kind {
             // The SerializedName annotation tells GSON how to convert
@@ -113,6 +127,111 @@ public class ZooDataItem {
             return null;
         }
 
+    }
+
+    /**
+     * Load ZooNode's from a JSON file (such as vertex_info.json).
+     *
+     * @param infoReader a reader from which to read the JSON input.
+     * @return a list
+     */
+    public static List<Exhibit> fromJson(Reader infoReader) {
+        var gson = new Gson();
+        var type = new TypeToken<List<Exhibit>>() {
+        }.getType();
+        return gson.fromJson(infoReader, type);
+    }
+
+    public static void toJson(List<Exhibit> infos, Writer writer) throws IOException {
+        var gson = new Gson();
+        var type = new TypeToken<List<Exhibit>>() {
+        }.getType();
+        gson.toJson(infos, type, writer);
+        writer.flush();
+        writer.close();
+    }
+
+    public enum Kind {
+        // The SerializedName annotation tells GSON how to convert
+        // from the strings in our JSON to this Enum.
+        @SerializedName("gate") GATE,
+        @SerializedName("exhibit") EXHIBIT,
+        @SerializedName("intersection") INTERSECTION,
+        @SerializedName("exhibit_group") EXHIBIT_GROUP;
+    }
+
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    @SerializedName("id")
+    @NonNull
+    public final String id;
+
+    @ColumnInfo(name = "group_id")
+    @SerializedName("group_id")
+    @Nullable
+    public final String groupId;
+
+    @ColumnInfo(name = "kind")
+    @SerializedName("kind")
+    @NonNull
+    public final Kind kind;
+
+    @ColumnInfo(name = "name")
+    @SerializedName("name")
+    @NonNull
+    public final String name;
+
+    @ColumnInfo(name = "tags")
+    @SerializedName("tags")
+    @NonNull
+    public final List<String> tags;
+
+    @ColumnInfo(name = "lat")
+    @SerializedName("lat")
+    public final Double lat;
+
+    @ColumnInfo(name = "lng")
+    @SerializedName("lng")
+    public final Double lng;
+
+    public boolean isExhibit() {
+        return kind.equals(Kind.EXHIBIT);
+    }
+
+    public boolean isIntersection() {
+        return kind.equals(Kind.INTERSECTION);
+    }
+
+    public boolean isGroup() {
+        return kind.equals(Kind.EXHIBIT_GROUP);
+    }
+
+    public boolean hasGroup() {
+        return groupId != null;
+    }
+
+    public ZooLocation getLocation() {
+        return new ZooLocation(this.lat, this.lng);
+    }
+
+    public ZooDataItem(@NonNull String id,
+                        @Nullable String groupId,
+                        @NonNull Kind kind,
+                        @NonNull String name,
+                        @NonNull List<String> tags,
+                        @Nullable Double lat,
+                        @Nullable Double lng) {
+        this.id = id;
+        this.groupId = groupId;
+        this.kind = kind;
+        this.name = name;
+        this.tags = tags;
+        this.lat = lat;
+        this.lng = lng;
+
+        if (!this.hasGroup() && (lat == null || lng == null)) {
+            throw new RuntimeException("Nodes must have a lat/long unless they are grouped.");
+        }
     }
 
 }
